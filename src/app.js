9e456648d6f930d7eab697dcafbe7940d224eb7e
Vanessa Yeung <vanessa@thejibe.com>
2021-11-08 14:13:58 -0800

    Add function to show current location data as default using geolocation:

--- src/app.js
+++ src/app.js
@@ -1,38 +1,73 @@
 let apiKey = config.API_KEY;
 
+//Gets location data and displays it using helper function
+
+let getCurrentLocationData = (res) => {
+  let latitude = res.coords.latitude;
+  let longitude = res.coords.latitude;
+  console.log(`Latitude: ${latitude}`);
+  console.log(`Longitude: ${longitude}`);
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(showCurrentLocationData);
+};
+
+let showCurrentLocationData = (res) => {
+  let rounded = Math.round(res.data.main.temp);
+  console.log(rounded);
+  temperatureElement.innerHTML = `${rounded}°C`;
+  cityElement.innerHTML = res.data.name;
+  conditionElement.innerHTML = res.data.weather[0].main;
+  humidityElement.innerHTML = `${res.data.main.humidity}%`;
+  windElement.innerHTML = `${res.data.wind.speed}km/h`;
+};
+
+let getCurrentLocation = () => {
+  navigator.geolocation.getCurrentPosition(getCurrentLocationData);
+};
+
+getCurrentLocation();
+
 // // Display the current date and time using JavaScript
-
 let now = new Date();
-
 let options = { weekday: "long" };
 let day = new Intl.DateTimeFormat("en-US", options).format(now);
-let time = now.getHours() + ":" + now.getMinutes();
-// let timeNow = `${day} ${time}`;
+let addZero = (i) => {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+};
 
-let currentTime = document.querySelector(".current-time");
-currentTime.innerHTML = day + " " + time;
+let time = now.getHours() + ":" + addZero(now.getMinutes());
+
+let timeElement = document.querySelector(".current-time");
+timeElement.innerHTML = day + " " + time;
+
+// Display Geolocated city time and weather by default
+let cityElement = document.querySelector(".current-city");
+let temperatureElement = document.querySelector(".current-temperature");
+let conditionElement = document.querySelector(".current-weather-condition");
+let humidityElement = document.querySelector(".current-humidity");
+let windElement = document.querySelector(".current-wind");
+let searchForm = document.querySelector("#weather-search");
 
 // // When searching for a city (i.e. Paris), display the city name and temperature on the page after the user submits the form
-let currentCity = document.querySelector(".current-city");
-let currentTemperature = document.querySelector(".current-temperature");
-let currentCondition = document.querySelector(".current-weather-condition");
-let currentHumidity = document.querySelector(".current-humidity");
-let currentWind = document.querySelector(".current-wind");
-let searchForm = document.querySelector("#weather-search");
 
 let showSearchedWeather = (res) => {
   console.log("res", res);
   let rounded = Math.round(res.data.main.temp);
-  currentTemperature.innerHTML = `${rounded}`;
-  currentCondition.innerHTML = res.data.weather[0].main;
-  currentHumidity.innerHTML = `${res.data.main.humidity}%`;
-  currentWind.innerHTML = `${res.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = `${rounded}`;
+  conditionElement.innerHTML = res.data.weather[0].main;
+  humidityElement.innerHTML = `${res.data.main.humidity}%`;
+  windElement.innerHTML = `${res.data.wind.speed}km/h`;
 };
 
 let handleSearch = (event) => {
   event.preventDefault();
   let input = document.querySelector("#search-input");
-  currentCity.innerHTML = input.value;
+  cityElement.innerHTML = input.value;
 
   let searchedCity = input.value;
 
